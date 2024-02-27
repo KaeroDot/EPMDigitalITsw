0c17fb9e5e2107fcbb78ba3a5bd6cc68730de5d0
KaeroDot <KaeroDot@contact.me.on.git.hub>
2024-02-27 09:15:21 +0100

    add: two calculation methods in CubicSpline (faster and slower)

--- algorithms_for_QWTB/alg_SplineResample/CubicSpline.m
+++ algorithms_for_QWTB/alg_SplineResample/CubicSpline.m
@@ -17,11 +17,19 @@ classdef CubicSpline
     methods
         function obj = CubicSpline(t, record)
             % Constructor for CubicSpline class.
+            % Check inputs
+            if numel(t) < 1
+                error('CubicSpline: empty time vector, cannot calculate splines on empty data!')
+            end
+            if numel(t) ~= numel(record)
+                error('CubicSpline: length of input time vector differs from input record vector!')
+            end
+            % Set object:
             obj.x = t;
             obj.y = record;
             obj.n = length(t);
             [obj.a, obj.b, obj.c, obj.d] = obj.calculateCoefficients();
-        end
+        end % function CubicSpline
 
         function [a, b, c, d] = calculateCoefficients(obj)
             % Calculate spline coefficients.
@@ -31,19 +39,19 @@ classdef CubicSpline
             d = zeros(1, obj.n);
             h = diff(obj.x);
             alpha = zeros(1, obj.n - 2);
-            % old code:
-                    % for i = 2:(obj.n - 1)
-                        % alpha(i-1) = (3 / h(i-1)) * (a(i + 1) - a(i)) - (3 / h(i-1)) * (a(i) - a(i - 1));
-                    % end
-            % faster code without for loop:
+
+            % Code represented as a loop (slow way): 
+            % for i = 2:(obj.n - 1)
+                % alpha(i-1) = (3 / h(i-1)) * (a(i + 1) - a(i)) - (3 / h(i-1)) * (a(i) - a(i - 1));
+            % end
+            % The same code, but faster and without for loop:
             a_ip1 = [ a  NaN NaN];
             a_i =   [NaN  a  NaN];
             a_im1 = [NaN NaN  a ];
             h_im1 = [NaN  h  NaN NaN]; % NaN two times because h is result of diff, that is shorter by 1 element
             alpha = (3 ./ h_im1) .* (a_ip1 - a_i) - (3 ./ h_im1) .* (a_i - a_im1);
             % remove excess NaNs:
-            % (if user will use empty x or y or both, next line will fail. But
-            % the scrip should check sane inputs at the beginning, not inside.)
+            % If x or y is empty, next line will fail:
             alpha = alpha(3 : end-2);
 
             l = ones(1, obj.n);
@@ -60,20 +68,64 @@ classdef CubicSpline
                 b(j) = (a(j + 1) - a(j)) / h(j) - h(j) * (c(j + 1) + 2 * c(j)) / 3;
                 d(j) = (c(j + 1) - c(j)) / (3 * h(j));
             end
-        end
+        end % function calculateCoefficients
 
-        function results = evaluate(obj, t2)
+        function y_eval = evaluate(obj, x_eval, varargin)
             % Evaluate the spline at given points.
-            results = zeros(1, length(t2));
-            for k = 1:length(t2)
-                x_eval = t2(k);
-                for i = 1:(obj.n - 1)
-                    if (obj.x(i) <= x_eval && x_eval <= obj.x(i + 1))
-                        dx = x_eval - obj.x(i);
-                        results(k) = obj.a(i) + obj.b(i) * dx + obj.c(i) * dx^2 + obj.d(i) * dx^3;
-                    end
-                end
+            %
+            % Parameters
+            % ----------
+            % x_eval: Array of double
+            %     X coordinates of evaluated points
+            %     fast_method: selects a method for calculation. If 1, faster method is selected. Default is 0.
+            %
+            % Returns
+            % -------
+            % y_eval: Array of double
+            %     Y coordinates of evaluated splines.
+
+            % Check inputs
+            if isempty(x_eval)
+                error('Empty new time vector. Cannot evaluate spline on empty data!')
             end
-        end
+            if nargin < 3
+                % Use arrayfun method as a failsafe.
+                fast_method = 0;
+            else
+                fast_method = varargin{1};
+                % ensure method is boolean:
+                fast_method = not(not(fast_method));
+            end
+
+            % Evaluate spline:
+            tic
+            if fast_method
+                % Slower method. Using arrayfun. Much faster than simple for cycles, 3x slower
+                % than faster method, but less memory demanding.
+            
+                x_eval = reshape(x_eval, [], 1); % Ensure x_eval is a column vector
+                y_eval = zeros(size(x_eval)); % Initialize y_eval with the same size as x_eval
+
+                indices = arrayfun(@(x) find(obj.x <= x, 1, 'last'), x_eval);
+                indices(indices < 1) = 1;
+                indices(indices > numel(obj.x) - 1) = numel(obj.x) - 1;
+
+                % Compute the spline polynomial for each segment
+                for i = 1:length(x_eval)
+                    dx = x_eval(i) - obj.x(indices(i))';
+                    y_eval(i) = obj.a(indices(i)) + obj.b(indices(i)) * dx + ...
+                                obj.c(indices(i)) * dx^2 + obj.d(indices(i)) * dx^3;
+                end
+            else
+                % Faster method. Using bsxfun.
+                indexes = reshape(sum(bsxfun(@le, obj.x(:), x_eval(:).'), 1), size(x_eval)); 
+                % Now size(indexes) is same as size(x_eval)
+                for k = 1:numel(x_eval)
+                    dx = x_eval(k) - obj.x(indexes(k));
+                    y_eval(k) = obj.a(indexes(k)) + obj.b(indexes(k)) * dx + obj.c(indexes(k)) * dx^2 + obj.d(indexes(k)) * dx^3;
+                end % for
+            end % if method
+            toc
+        end % function evaluate
     end
 end
