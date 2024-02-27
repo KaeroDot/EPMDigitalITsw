e55dd5f7857264f56e4070d140c738bfe4326f97
KaeroDot <KaeroDot@contact.me.on.git.hub>
2024-02-27 14:46:47 +0100

    add: improved test for spline method

--- algorithms_for_QWTB/alg_SplineResample/TestQubicSpline.m
+++ algorithms_for_QWTB/alg_SplineResample/TestQubicSpline.m
@@ -1,28 +1,32 @@
-f = 49.77;
-f_est = f;
-N = 2000;
-ts = 0.00025;
-t = (0:N-1)*ts;
-record = sin(2*pi*f*t);
+f = 49.77;          % signal frequency
+f_est = f;          % estimate of signal frequency
+N = 1e3;            % number of samples in the record
+method = 0;         % 0 for arrayfun, 1 for bsxfun
+ts = 0.00025;       % sampling period
+t = (0:N-1)*ts;     % timestamps
+record = sin(2*pi*f*t); % record - sampled values
 
 %% resampling parameters
 % so that number of samples remain the same and the resulting record is
 % sampled coherently
 %N = length(Record);
-Fs = 1/ts;
+Fs = 1/ts;              % sampling frequency
 ssfr1 = Fs / f_est;     % the actual ratio
-Cycles = floor(N/ssfr1);        % return number of full cycles available
-ts2 = (Cycles/f_est)/N;
-Fs2 = 1/ts2; % dersired new sampling frequency
-t2 = (0:N-1)*ts2;
+Cycles = floor(N/ssfr1); % return number of full cycles available
+ts2 = (Cycles/f_est)/N; % new sampling period
+Fs2 = 1/ts2;            % desired new sampling frequency
+t2 = (0:N-1)*ts2;       % new timestamps
 
 %% perform resampling
-spline = CubicSpline(t, record);
-record2 = spline.evaluate(t2);
+spline = CubicSpline(t, record);    % calculate spline coefficients
+tic
+record2 = spline.evaluate(t2, method); % resample
+toc
 
-X  = Spectra(record,ts,'none');
-X2 = Spectra(record2,ts2,'none');
-plot(X.f,X.LSdB)
+X  = Spectra(record,ts,'none');     % do spectrum of input record
+X2 = Spectra(record2,ts2,'none');   % do spectrum of resampled record
+figure
 hold on
+plot(X.f,X.LSdB)                    
 plot(X2.f,X2.LSdB)
 hold off
